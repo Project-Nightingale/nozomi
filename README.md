247ce5b74420873ca20cbe8082a1d973f2c87e80
AKB428 <otoraru@gmail.com>
2014-12-31 05:32:24 +0900

    1fix

--- README.md
+++ README.md
@@ -4,9 +4,14 @@ cp config/sample.app.conf.json config/app.conf.json
 ## 起動に必要なもの
 
 (オブジェクトストレージを使用する場合)
+
 memcached サーバー = オブジェクトストレージの鍵を格納するのに使用
 
 ## 起動
 
-bundle exec ruby nozomi.rb　-p 80 -e production
+bundle exec ruby app.rb　-p 80 -e production
+
+## 開発
+
+bundle exec rerun ruby app.rb
 
--- app.rb
+++ app.rb
@@ -12,6 +12,10 @@ get '/' do
   haml :index
 end
 
+get '/index.html' do
+  haml :index
+end
+
 # アップロード処理
 post '/upload' do
   if params[:file]
@@ -21,7 +25,7 @@ post '/upload' do
     File.open(save_path, 'wb') do |f|
       p params[:file][:tempfile]
       f.write params[:file][:tempfile].read
-      @mes = "アップロード成功 " + save_path
+      @mes = "アップロード成功"
 
       @file_url = send_object_strage(save_path)
     end
@@ -33,29 +37,42 @@ end
 
 
 def send_object_strage(src_path)
-  object_strage_conf_path = './config/object_strage.json'
-  File.open object_strage_conf_path do |file|
-    conf = JSON.load(file.read)
-    @swift = conf['swift']
+  if @swift.nil?
+    object_strage_conf_path = './config/object_strage.json'
+    puts "LOAD " + object_strage_conf_path;
+    File.open object_strage_conf_path do |file|
+      conf = JSON.load(file.read)
+      @swift = conf['swift']
+    end
   end
 
-  app_conf_path = './config/app.json'
-  File.open app_conf_path do |file|
-    conf = JSON.load(file.read)
-    @object_strage = conf['object_strage']
+  if @object_strage.nil?
+    app_conf_path = './config/app.json'
+    puts "LOAD " + app_conf_path;
+    File.open app_conf_path do |file|
+      conf = JSON.load(file.read)
+      @object_strage = conf['object_strage']
+    end
   end
 
   rabbit_swift_client = RabbitSwift::Client.new(@swift);
   token = rabbit_swift_client.get_token
   dest_path = @swift["endPoint"] + "/" + @object_strage["container"]
 
+  # TODO ファイルのリネーム
+  # TODO Masterサーバーに問い合わせ+クライアントIPなどの情報を送信
+  # TODO Masterサーバー側で情報のDB登録
+  # TODO 戻ってきた名前でファイルリネーム
+  # TODO IP回数制限などのエラーが出た場合通知がくるのでハンドリングする
+
+
   status = rabbit_swift_client.upload(token, dest_path, src_path)
   if (status == RabbitSwift::Client::UPLOAD_SUCCESS_HTTP_STATUS_CODE)
-     //TODO ファイル削除
+     #TODO ファイル削除
   else
 
   end
 
-  filename = "";
-  @object_strage + "/" + filename;
+  filename = File::basename(src_path);
+  @object_strage['web_url'] + filename;
 end
