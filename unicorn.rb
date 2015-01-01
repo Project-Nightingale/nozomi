f1dda56181755ddbdb87bd36d30350462a67b481
AKB428 <otoraru@gmail.com>
2015-01-02 02:03:22 +0900

    fix .gitignore

--- unicorn.rb
+++ unicorn.rb
@@ -5,8 +5,8 @@ working_directory @dir
 
 timeout 300
 
-pid "#{@dir}tmp/pids/unicorn.pid" #pidを保存するファイル
+pid "#{@dir}tmp/unicorn.pid" #pidを保存するファイル
 
 # unicornは標準出力には何も吐かないのでログ出力を忘れずに
-stderr_path "#{@dir}log/unicorn.stderr.log"
-stdout_path "#{@dir}log/unicorn.stdout.log"
+#stderr_path "#{@dir}log/unicorn.stderr.log"
+#stdout_path "#{@dir}log/unicorn.stdout.log"
