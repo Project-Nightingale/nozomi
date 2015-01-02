fcd5e382d0973451d1476c757810d37cf3d502a7
root <root@www3206up.sakura.ne.jp>
2015-01-02 09:14:06 +0900

    fix

--- unicorn.rb
+++ unicorn.rb
@@ -5,7 +5,7 @@ working_directory @dir
 
 timeout 300
 
-pid File.join(@dir,"tmp/unicorn.pid") #pidを保存するファイル
+pid "/tmp/unicorn.pid" #pidを保存するファイル
 
 # unicornは標準出力には何も吐かないのでログ出力を忘れずに
 #stderr_path "#{@dir}log/unicorn.stderr.log"
